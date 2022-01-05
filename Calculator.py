0ad4739a752013c2ecfcd5f942e0e9c999f75acc
Shiva <shivavisit28@gmail.com>
2022-01-05 08:24:38 +0200

    Pi Programs

---
+++ Calculator.py
@@ -0,0 +1,16 @@
+n = input()
+a = int(input())
+b = int(input())
+
+if n == "+":
+    print(a+b)
+elif n =="-":
+    print(a-b)
+elif n =="*":
+    print(a*b)
+elif n =="/":
+    print(a/b)
+elif n =="%":
+    print(a%b)
+else:
+    print("Invalid Input")
---
+++ Sum_Of_Digits.py
@@ -0,0 +1,25 @@
+n = input()
+
+nlen = len(n)
+
+
+if nlen==1:
+   n0 =int(n[0])
+   print(n0)
+elif nlen==2:
+    n0 =int(n[0])
+    n1 =int(n[1])
+    print(n0+n1)
+elif nlen==3:
+    n0 =int(n[0])
+    n1 =int(n[1])
+    n2 =int(n[2])
+    print(n0+n1+n2)
+elif nlen==4:
+    n0 =int(n[0])
+    n1 =int(n[1])
+    n2 =int(n[2])
+    n3 =int(n[3])
+    print(n0+n1+n2+n3)
+else:
+    print("Invalid Range")
