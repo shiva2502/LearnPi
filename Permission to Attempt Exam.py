09c8f8b5df31bd132a6c2bdada1f6200073745dd
Shiva <shivavisit28@gmail.com>
2022-01-05 18:22:06 +0200

    second commit

---
+++ Permission to Attempt Exam.py
@@ -0,0 +1,16 @@
+p = input()
+m = input()
+
+p1 = p[0]
+p2 = p[1]
+ap = p1+p2
+ap = int(ap)
+
+if (ap >= 75):
+    print("Allowed to write exam")
+elif (ap < 75 and m =="Y"):
+    print("Allowed to write exam")
+elif (ap < 75 and m =="N"):    
+    print("Cannot write exam")
+else:
+    print("Invalid input")
